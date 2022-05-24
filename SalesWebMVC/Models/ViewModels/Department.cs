0580cad6b5c076aba1c296373b9b21d9775f547a
ronxss <ronsilva768@gmail.com>
2022-05-23 21:48:53 -0300

    First MVC Department

---
+++ SalesWebMVC/Models/ViewModels/Department.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMVC.Models.ViewModels
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+}
