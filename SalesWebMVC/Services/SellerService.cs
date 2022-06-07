40e8c1380ab1598302294ebd671a051c9f2c86d2
ronxss <ronsilva768@gmail.com>
2022-06-07 01:33:21 -0300

    SellerService and basic FindAll

---
+++ SalesWebMVC/Services/SellerService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using SalesWebMVC.Models;
+using System.Linq;
+
+namespace SalesWebMVC.Services
+
+{
+    public class SellerService
+    {
+        private readonly SalesWebMVCContext _context;
+
+        public SellerService(SalesWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
