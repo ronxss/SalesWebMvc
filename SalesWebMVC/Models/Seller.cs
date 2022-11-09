193cab9e2ac0378eb2c02390e183f5ad16e3d278
ronxss <ronsilva768@gmail.com>
2022-11-09 17:21:40 -0300

    Foreign key not null (referential integrity)

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -28,6 +28,16 @@ namespace SalesWebMVC.Models
             BaseSalary = baseSalary;
             Department = department;
         }
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department, int departmentId)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Department = department;
+            DepartmentId = departmentId;
+        }
 
         public void AddSales(SalesRecord sr)
         {
