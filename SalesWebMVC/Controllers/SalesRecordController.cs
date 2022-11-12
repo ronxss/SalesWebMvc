ccc520b4b8558039da3f462076ec3e192b5f7af5
ronxss <ronsilva768@gmail.com>
2022-11-12 11:05:04 -0300

    Preparing sales search navigation views

---
+++ SalesWebMVC/Controllers/SalesRecordController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Controllers
+{
+    public class SalesRecordController : Controller
+    {
+        public readonly SalesRecordService _salesRecordService;
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate)
+        {
+            var list = await _salesRecordService.FindByDateAsync(minDate, maxDate);
+            return View(list);
+        }
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+
+    }
+}
